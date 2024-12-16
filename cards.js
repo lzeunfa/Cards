543ccb08a2c547c2408c6c4885329215633ee505
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-16 20:36:40 -0300

    Criei o exercicio usando JS

---
+++ cards.js
@@ -0,0 +1,43 @@
+const card1 = document.querySelector(`div.card1`)
+
+const card2 = document.querySelector(`div.card2`)
+
+const card3 = document.querySelector(`div.card3`)
+
+const card4 = document.querySelector(`div.card4`)
+
+//para o card cinza 1
+function expandir1(){
+    card1.style.width=`40%`
+}
+
+function normal1(){
+    card1.style.width=`25%`
+}
+
+//para o card azul 2
+function expandir2(){
+    card2.style.width=`40%`
+}
+
+function normal2(){
+    card2.style.width=`25%`
+}
+
+//para o card verde 3
+function expandir3(){
+    card3.style.width=`40%`
+}
+
+function normal3(){
+    card3.style.width=`25%`
+}
+
+//para o card salmão 4
+function expandir4(){
+    card4.style.width=`40%`
+}
+
+function normal4(){
+    card4.style.width=`25%`
+}
